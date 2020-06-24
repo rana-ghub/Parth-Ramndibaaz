6468b3d4c366ae2a3c638769e8160c24a9a2ffa6
rana-ghub <ssshashankrana001@gmail.com>
2020-06-24 12:30:51 +0530

    Updates

---
+++ precache-manifest.4fa13cde5f374ec96748557431751ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc9159621ecc9bf0835c35d12e2a2f6",
+    "url": "/parth-ramndibaaz/index.html"
+  },
+  {
+    "revision": "2b580660170d7086d344",
+    "url": "/parth-ramndibaaz/static/css/main.157f68f6.chunk.css"
+  },
+  {
+    "revision": "e13362c5f41e0542c17e",
+    "url": "/parth-ramndibaaz/static/js/2.9d0f79ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parth-ramndibaaz/static/js/2.9d0f79ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b580660170d7086d344",
+    "url": "/parth-ramndibaaz/static/js/main.e1ba12c4.chunk.js"
+  },
+  {
+    "revision": "3d9f8c4a41f6d68f8cdb",
+    "url": "/parth-ramndibaaz/static/js/runtime-main.5078ab27.js"
+  }
+]);
